08b8d6266bba6bb139c6f01a7994dd10a2377d9a
Owen Smith <o.smith.1@estdiantec.cr>
2025-11-05 23:59:51 -0600

    Se agrega funcion para crear aleatoriamente un grafo junto con sus funciones extra para separar logicas y que sea mas claro de leer, y se agrega atributo a grafo para verificar si es completo o no aunque se esta viendo si se implementa o no segun lo que diga el profesor al respecto

--- prototipo.js
+++ prototipo.js
@@ -13,6 +13,8 @@ class Grafo {
     this.nodos = [];
     this.k = null;
     this.listaColores = [];
+    this.largo = 0
+    this.completo = false;
   }
 
   asignarK(n){
@@ -22,10 +24,12 @@ class Grafo {
   agregarNodo(valor) {
     const nodo = new Nodo(valor);
     this.nodos.push(nodo);
+    this.largo += 1;
     return nodo;
   }
 
   agregarColores(){
+    this.listaColores = [];
     for (let i = 0; i < this.k ; i++){
       const color = `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, "0").toUpperCase()}`;
       this.listaColores.push(color)
@@ -46,13 +50,66 @@ class Grafo {
     return false;
   }
 
+  verificarCompleto(){
+  }
+}
 
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function verificarExistenciaArista(nodo1,nodo2,lista){
+  for (let i = 0; i < lista.length ; i++){
+    if (lista[i][0].valor === nodo1.valor && lista[i][1].valor === nodo2.valor){
+      return true;
+    } 
+    if (lista[i][0].valor === nodo2.valor && lista[i][1].valor === nodo1.valor){
+      return true;
+    } 
+  }
+
+  return false;
 }
 
 // Aun en desarrollo
 function crearGrafoAleatorio(cantidadNodos,kColores,){
   const grafo = new Grafo();
+  // Creamos las instancias de nodos
+  for (let i = 1; i < cantidadNodos + 1; i++){
+    grafo.agregarNodo(i);
+  }
+  // Generamos los colores
+  grafo.asignarK(kColores);
+  grafo.agregarColores();
 
+  // Creamos las conneciones entre nodos (aristas)
+  let listaConexiones = []
+
+  while (grafo.verificarAislado()){ // Mientras que exista un nodo aislado en el grafo ...
+    let nodo1 = grafo.nodos[random(0,grafo.largo - 1)]
+    let nodo2 = grafo.nodos[random(0,grafo.largo - 1)]
+
+    // Validamos que los nodos escogidos no sean los mismos
+    // Validamos que no exista ya una conexion entre esos mismos dos nodos.
+    if (nodo1 != nodo2 && verificarExistenciaArista(nodo1,nodo2,listaConexiones) === false){
+      grafo.agregarArista(nodo1,nodo2);
+      let conexion = [nodo1,nodo2]
+      listaConexiones.push(conexion)
+    }
+
+  }
+  console.log("Grafo creado con exito!")
+
+  return grafo;
 }
 
 
+let grafo = crearGrafoAleatorio(120,5);
+
+
+console.log("=== Conexiones del Grafo ===");
+grafo.nodos.forEach(nodo => {
+  const vecinos = nodo.vecinos.map(v => v.valor).join(", ");
+  console.log(`Nodo ${nodo.valor}: ${vecinos.length > 0 ? vecinos : "sin conexiones"}`);
+});
